13cf42eaaf54302f0ff62f1e963cf6fdb523e16a
Caleb Cramer <calebcramer@Calebs-MacBook-Pro.local>
2019-10-24 21:08:19 -0700

    added a reset dialog

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,35 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Controller {
+    Model model;
+    View view;
+
+    public Controller(Model model){
+        this.model = model;
+        this.view = new View(this);
+        view.resetGame();
+
+
+        //if still only one action req change to "view.resetButton.addActionListener(actionEvent -> view.resetGame);
+        view.resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (JOptionPane.showConfirmDialog(null,
+                        "This will end the game and set the win/loss stats to 0. Are you sure?",
+                        "Reset names and stats",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE)==0)
+                    view.resetGame();
+            }
+        });
+    }
 
 
 
     public static void main(String[] args) {
-        Controller myController = new Controller();
-        View myview = new View(myController);
-        myview.resetGame();
+        Model model = new Model();
+        Controller myController = new Controller(model);
     }
 }
