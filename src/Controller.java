835483700308f7c5df1e784577212277ebc150a0
Caleb Cramer <calebcramer@Calebs-MacBook-Pro.local>
2019-10-24 20:41:11 -0700

    GUI done

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,10 @@
 public class Controller {
+
+
+
     public static void main(String[] args) {
         Controller myController = new Controller();
         View myview = new View(myController);
+        myview.resetGame();
     }
 }
--- src/View.java
+++ src/View.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class View extends JFrame {
     protected Controller controller;
     protected JButton resetButton; //south
-    protected JPanel scoresPanel; //north
+    protected JPanel scoreArea; //north
     protected JPanel gameGrid; //center
+    private JButton panel_1_1;
+    private JButton panel_1_2;
+    private JButton panel_1_3;
+    private JButton panel_2_1;
+    private JButton panel_2_2;
+    private JButton panel_2_3;
+    private JButton panel_3_1;
+    private JButton panel_3_2;
+    private JButton panel_3_3;
+    private JLabel name1;
+    private JLabel wins1;
+    private JLabel losses1;
+    private JLabel name2;
+    private JLabel wins2;
+    private JLabel losses2;
+
+
 
     public View(Controller c){
         super("Tic Tac Toe");
@@ -16,7 +32,7 @@ public class View extends JFrame {
         setupUI();
         pack();
     }
-    public void setupUI(){
+    private void setupUI(){
         setVisible(true);
         setPreferredSize(new Dimension(400,400));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,38 +54,48 @@ public class View extends JFrame {
                 new Font("Arial", Font.PLAIN, 25));
         gameGrid.setBorder(playerUpdate);
         gameGrid.setLayout(new GridLayout(3,3));
-        JButton panel_1_1 = new JButton("1_1");
+
+        panel_1_1 = new JButton("1_1");
         panel_1_1.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_1_1);
-        JButton panel_1_2 = new JButton("1_2");
+
+        panel_1_2 = new JButton("1_2");
         panel_1_2.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_1_2);
-        JButton panel_1_3 = new JButton("1_3");
+
+        panel_1_3 = new JButton("1_3");
         panel_1_3.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_1_3);
-        JButton panel_2_1 = new JButton("2_1");
+
+        panel_2_1 = new JButton("2_1");
         panel_2_1.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_2_1);
-        JButton panel_2_2 = new JButton("2_2");
+
+        panel_2_2 = new JButton("2_2");
         panel_2_2.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_2_2);
-        JButton panel_2_3 = new JButton("2_3");
+
+        panel_2_3 = new JButton("2_3");
         panel_2_3.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_2_3);
-        JButton panel_3_1 = new JButton("3_1");
+
+        panel_3_1 = new JButton("3_1");
         panel_3_1.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_3_1);
-        JButton panel_3_2 = new JButton("3_2");
+
+        panel_3_2 = new JButton("3_2");
         panel_3_2.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_3_2);
-        JButton panel_3_3 = new JButton("3_3");
+
+        panel_3_3 = new JButton("3_3");
         panel_3_3.setFont(new Font("Arial", Font.BOLD, 40));
         gameGrid.add(panel_3_3);
+
         contentPanel.add(gameGrid,BorderLayout.CENTER);
         //end center area
 
         //North area
-        JPanel scoreArea = new JPanel();
+        scoreArea = new JPanel();
         scoreArea.setLayout(new GridLayout(0,2));
 
         JLabel nameTitle1 = new JLabel("Name: ");
@@ -80,29 +106,29 @@ public class View extends JFrame {
         JLabel lossesTitle2 = new JLabel("Losses: ");
 
         JPanel player1Score = new JPanel();
-        player1Score.setBorder(BorderFactory.createTitledBorder("PLAYER 1(X)"));
+        player1Score.setBorder(BorderFactory.createTitledBorder("Player 1(X)"));
         player1Score.setLayout(new GridLayout(3,2));
         player1Score.add(nameTitle1);
-        JLabel name1 = new JLabel();
+        name1 = new JLabel();
         player1Score.add(name1);
         player1Score.add(winsTitle1);
-        JLabel wins1 = new JLabel();
+        wins1 = new JLabel();
         player1Score.add(wins1);
         player1Score.add(lossesTitle1);
-        JLabel losses1 = new JLabel();
+        losses1 = new JLabel();
         player1Score.add(losses1);
 
         JPanel player2Score = new JPanel();
-        player2Score.setBorder(BorderFactory.createTitledBorder("PLAYER 2(O)"));
+        player2Score.setBorder(BorderFactory.createTitledBorder("Player (O)"));
         player2Score.setLayout(new GridLayout(3,2));
         player2Score.add(nameTitle2);
-        JLabel name2 = new JLabel();
+        name2 = new JLabel();
         player2Score.add(name2);
         player2Score.add(winsTitle2);
-        JLabel wins2 = new JLabel();
+        wins2 = new JLabel();
         player2Score.add(wins2);
         player2Score.add(lossesTitle2);
-        JLabel losses2 = new JLabel();
+        losses2 = new JLabel();
         player2Score.add(losses2);
 
         scoreArea.add(player1Score);
@@ -112,5 +138,27 @@ public class View extends JFrame {
         //end north area
 
         getContentPane().add(contentPanel);
+    }
+
+    public void resetGame(){
+        String player1Name = JOptionPane.showInputDialog("Enter Player 1's name here");
+        name1.setText(player1Name);
+        wins1.setText("0");
+        losses1.setText("0");
+
+        String player2Name = JOptionPane.showInputDialog("Enter Player 2's name here");
+        name2.setText(player2Name);
+        wins2.setText("0");
+        losses2.setText("0");
+
+        panel_1_1.setText("");
+        panel_2_1.setText("");
+        panel_3_1.setText("");
+        panel_1_2.setText("");
+        panel_2_2.setText("");
+        panel_3_2.setText("");
+        panel_1_3.setText("");
+        panel_2_3.setText("");
+        panel_3_3.setText("");
     }
 }
